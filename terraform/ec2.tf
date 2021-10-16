a38537fc75bea28dc603ab54e2a0c11d3967095d
Manita Kushwaha <manitaen1051@gmail.com>
2021-10-16 05:33:15 +0530

    auto pushing chainges repo

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,16 +1,16 @@
-resource "aws_spot_instance_request" "RoboShop" {
-  count                  = local.LENGTH
+#resource "aws_spot_instance_request" "RoboShop" {
+#  count                  = local.LENGTH
 #  ami                    = "ami-074df373d6bafa625"
 #  spot_price             = "0.0031"
 #  instance_type          = "t3.micro"
 #  vpc_security_group_ids = ["sg-0342f3ac50a4bc8c6"]
 #  wait_for_fulfillment   = true
 #  //  spot_type              = "persistent"
-
-  tags                   = {
-    Name                 = element(var.COMPONENTS, count.index)
-  }
-}
+#
+#  tags                   = {
+#    Name                 = element(var.COMPONENTS, count.index)
+#  }
+#}
 
 resource "aws_ec2_tag" "name-tag" {
   count                  = local.LENGTH
