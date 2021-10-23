df8136df762c9e8cc07346538f122b1508676f90
Manita Kushwaha <manitaen1051@gmail.com>
2021-10-24 01:59:20 +0530

    auto pushing chainges repo

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -8,20 +8,20 @@ resource "aws_spot_instance_request" "RoboShop" {
   //  spot_type              = "persistent"
 
   tags                   = {
-    Name                 = element(var.COMPONENTS, count.index)
+    Name                 = "${element(var.COMPONENTS, count.index)}-${var.ENV}"
   }
 }
 
-resource "aws_ec2_tag" "name-tag" {
-  count                  = local.LENGTH
-  resource_id            = element(aws_spot_instance_request.RoboShop.*.spot_instance_id, count.index)
-  key                    = "Name"
-  value                  = element(var.COMPONENTS, count.index)
-}
+#resource "aws_ec2_tag" "name-tag" {
+#  count                  = local.LENGTH
+#  resource_id            = element(aws_spot_instance_request.RoboShop.*.spot_instance_id, count.index)
+#  key                    = "Name"
+#  value                  = element(var.COMPONENTS, count.index)
+#}
 
 resource "aws_route53_record" "records" {
   count   = local.LENGTH
-  name    = element(var.COMPONENTS, count.index)
+  name    = "${element(var.COMPONENTS, count.index)}-${var.ENV}"
   type    = "A"
   zone_id = "Z02807362V1O6GIFPEOSL"
   ttl     = 300
@@ -36,6 +36,6 @@ locals {
 
 resource "local_file" "inventory-file" {
   content = "[FRONTEND]\n${aws_spot_instance_request.RoboShop.*.private_ip[9]}\n[PAYMENT]\n${aws_spot_instance_request.RoboShop.*.private_ip[8]}\n[USER]\n${aws_spot_instance_request.RoboShop.*.private_ip[7]}\n[CATALOGUE]\n${aws_spot_instance_request.RoboShop.*.private_ip[6]}\n[SHIPPING]\n${aws_spot_instance_request.RoboShop.*.private_ip[5]}\n[CART]\n${aws_spot_instance_request.RoboShop.*.private_ip[4]}\n[REDIS]\n${aws_spot_instance_request.RoboShop.*.private_ip[3]}\n[RABBITMQ]\n${aws_spot_instance_request.RoboShop.*.private_ip[2]}\n[MONGODB]\n${aws_spot_instance_request.RoboShop.*.private_ip[1]}\n[MYSQL]\n${aws_spot_instance_request.RoboShop.*.private_ip[0]}"
-  filename = "/tmp/inv-roboshop"
+  filename = "/tmp/inv-roboshop-${var.ENV}"
 }
 
